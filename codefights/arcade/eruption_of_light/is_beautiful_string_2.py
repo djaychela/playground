8f9ecbb0bc22f6364ea8910a19f3d8944439dba6
Darren Jones <djaychela@gmail.com>
2017-12-02 08:52:47 +0000

    Improved version with more readable code and list comprehension.

--- codefights/arcade/eruption_of_light/is_beautiful_string_2.py
+++ codefights/arcade/eruption_of_light/is_beautiful_string_2.py
@@ -1,13 +1,10 @@
 import string
 
 def isBeautifulString(input_string):
-    input_list=list(input_string)
-    input_list=sorted(input_list)
-    counts = []
-    for i in range(97,123):
-        counts.append(input_list.count(chr(i)))
+    input_list=sorted(list(input_string))
+    counts = [input_list.count(chr(i)) for i in range(97,123)]
     for i in range(1,26):
-        if counts[i]>counts[i-1]:
+        if counts[i] > counts[i-1]:
             return False
     return True
 
@@ -17,8 +14,8 @@ def isBeautifulString_2(inputString):
     r = [inputString.count(i) for i in string.ascii_lowercase]
     return r[::-1] == sorted(r)
 
-print(isBeautifulString_2("bbbaacdafe"))
-print(isBeautifulString_2("aabbb"))
-print(isBeautifulString_2("bbc"))
+print(isBeautifulString("bbbaacdafe"))
+print(isBeautifulString("aabbb"))
+print(isBeautifulString("bbc"))
 
 
